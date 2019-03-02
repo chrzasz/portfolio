6705159043ba1d52de14632b2737c528aab4d6a0
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-03-02 11:32:19 +0100

    js concat

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,48 @@
-console.log(123 + "test");
-console.log(" java jest slaba ".replace("slaba", "mocna").repeat(3));
-console.log(" JAVA   jest      mocno    typowany     ".trim().split(/\s+/));
-console.log(
-   " JAVA   jest      mocno    typowany     "
-      .trim()
-      .split(" ")
-      .filter(Boolean)
-);
+// console.log(123 + "test");
+// console.log(" java jest slaba ".replace("slaba", "mocna").repeat(3));
+// console.log(" JAVA   jest      mocno    typowany     ".trim().split(/\s+/));
+// console.log(
+//    " JAVA   jest      mocno    typowany     "
+//       .trim()
+//       .split(" ")
+//       .filter(Boolean)
+// );
 
-console.log((234).toString(2));
+// console.log((234).toString(2));
 
-console.log((23.2345432).toPrecision(5));
-console.log((23.2345432).toFixed(3));
+// console.log((23.2345432).toPrecision(5));
+// console.log((23.2345432).toFixed(3));
 
-console.log(parseFloat("54.34test"));
-console.log(isFinite(2e134));
+// console.log(parseFloat("54.34test"));
+// console.log(isFinite(2e134));
 
-console.log(!!0);
+// console.log(!!0);
 
-console.log(5 > 1);
+// console.log(5 > 1);
 
-console.log(0 || false || "abc" || 23);
+// console.log(0 || false || "abc" || 23);
 
-console.log("abc" && 23 && "last");
+// console.log("abc" && 23 && "last");
 
-var str = "234";
-var bin = (+str).toString(2);
-console.log(bin);
+// var str = "234";
+// var bin = (+str).toString(2);
+// console.log(bin);
 
-const b = "";
+// const b = "";
 
-let count = 0;
-console.log(count);
-let result;
-result = 1;
-console.log(result);
+// let count = 0;
+// console.log(count);
+// let result;
+// result = 1;
+// console.log(result);
 
-const expenses = 250;
-const myName = "Grzegorz";
-const amount = 1000;
+// const expenses = 250;
+// const myName = "Grzegorz";
+// const amount = 1000;
 
-console.log(amount - expenses);
+// console.log(amount - expenses);
+
+const story = "This is sample story";
+const newStory = "This is awsome new story";
+console.log(story.concat(" ", newStory));
+console.log(`${story}`.concat(" ", newStory));
